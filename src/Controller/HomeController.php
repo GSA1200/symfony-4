65db86323f99da4cd5d6f8b5eda412a30c6fdd24
GSA1200 <yvan.kurdijaka@gmail.com>
2019-05-16 14:42:57 +0200

    Premier commit installation et pade d'accueil

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class HomeController extends Controller
+{
+    /**
+     * @Route("/hello/{prenom}/{age}", name="hello")
+     */
+    public function hello($prenom="anonyme",$age=0){
+        return $this->render(
+                'home/hello.html.twig',
+                [
+                    'prenom'=>$prenom,
+                    'age'=>$age
+                ]
+                );
+    }
+
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function home()
+    {
+        $prenoms=["Yvan"=>51,"Ingrid"=>50,"Lili"=>3];
+        
+        return $this->render(
+                'home/home.html.twig',[
+                    'title'=>"Bonjour à tous",
+                    'content'=>"Contenu de mon article",
+                    'age'=>17,
+                    'prenoms'=>$prenoms
+                ]
+                );
+    }
+}
