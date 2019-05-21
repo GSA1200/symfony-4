d98b69596c1d0c6dfeb0b959b6f33ac97b65e422
GSA1200 <yvan.kurdijaka@gmail.com>
2019-05-21 15:23:43 +0200

    Gestion des annonces, formulaires de creation et edition

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,11 +2,11 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
-class HomeController extends Controller
+class HomeController extends AbstractController
 {
     /**
      * @Route("/hello/{prenom}/{age}", name="hello")
